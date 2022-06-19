d776e671d04bd57507150c72a7f18db2917a8d41
Franklin <franklinjunior12@hotmail.com>
2022-06-19 11:24:39 -0300

    bunch of stuff

--- src/iot_configs.h
+++ src/iot_configs.h
@@ -2,13 +2,13 @@
 // SPDX-License-Identifier: MIT
 
 // Wifi
-#define IOT_CONFIG_WIFI_SSID "NET_2GC50515"
-#define IOT_CONFIG_WIFI_PASSWORD "ECC50515"
+#define IOT_CONFIG_WIFI_SSID ""
+#define IOT_CONFIG_WIFI_PASSWORD ""
 
 // Azure IoT
-#define IOT_CONFIG_IOTHUB_FQDN "riverIoT.azure-devices.net"
-#define IOT_CONFIG_DEVICE_ID "riverdatalogger1"
-#define IOT_CONFIG_DEVICE_KEY "ahhe1mjD6IOXqzTMe45936vKX8NZrcMDj3chBTktvIk="
+#define IOT_CONFIG_IOTHUB_FQDN ""
+#define IOT_CONFIG_DEVICE_ID ""
+#define IOT_CONFIG_DEVICE_KEY ""
 
 // Publish 1 message every 2 seconds
 #define TELEMETRY_FREQUENCY_MILLISECS 2000
